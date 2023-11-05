365806ac1fdffd84bbb7eb574d7bb2e9da4dbe87
CyberCaban <andrewgamer63@gmail.com>
2023-11-05 20:39:33 +0300

    playable game (kinda)

--- server/server.js
+++ server/server.js
@@ -28,6 +28,173 @@ app.use(cors());
 
 app.use("/play", play);
 
+function socketConnect(socket) {
+  socket.on("joinRoom", (data) => {
+    JoinRoom(socket, data);
+  });
+
+  socket.on("disconnect", () => {
+    socketDisconnect(socket);
+  });
+}
+
+function JoinRoom(socket, data) {
+  const { username, room } = data;
+
+  const usersInRoom = allUsers.filter((user) => user.room === room);
+
+  if (usersInRoom.length < 2) {
+    //add a button to pick x or o
+
+    socket.join(room);
+    allUsers.push({ id: socket.id, username, room });
+
+    const timestamp = Date.now();
+
+    //сообщение всем участникам комнаты, что юзер присоединился
+    socket.to(room).emit("messageReceive", {
+      message: `${username} has joined`,
+      username: "server",
+      timestamp,
+    });
+
+    //приветствие
+    socket.emit("messageReceive", {
+      message: `Welcome ${username}`,
+      username: "server",
+      timestamp,
+    });
+  } else {
+    socket.emit("Error", { message: "can't join the room" });
+  }
+
+  if (allUsers.filter((user) => user.room === room).length === 2) {
+    const usersInRoom = allUsers.filter((user) => user.room === room);
+    // console.log(usersInRoom);
+    const user1 = usersInRoom[0];
+    const user2 = usersInRoom[1];
+    io.to([user1.id, user2.id]).emit("showPickASideComponent");
+  }
+
+  socket.on("pickASide", (data) => {
+    PickASide(socket, data, room);
+  });
+
+  socket.on("turn", (data) => {
+    Turn(socket, data);
+  });
+
+  socket.on("sendMessage", (data) => {
+    SendMessage(socket, data);
+  });
+
+  //devInfo
+  socket.on("devInfo", () => {
+    socket.emit("devInfo", allUsers);
+    // console.log(allUsers);
+  });
+}
+
+function PickASide(socket, data, room) {
+  const user1 = allUsers.filter(
+    (user) => user.room === room && user.id === socket.id
+  )[0];
+  const user2 = allUsers.filter(
+    (user) => user.room === room && user.id !== socket.id
+  )[0];
+  user1.side = data.side;
+
+  switch (data.side) {
+    case "X":
+      user2.side = "O";
+      break;
+    case "O":
+      user2.side = "X";
+      break;
+  }
+
+  io.to([user1.room, user2.room]).emit("showPickASideComponent");
+  io.to(user1.id).emit("sidePick", { side: user1.side });
+  io.to(user2.id).emit("sidePick", { side: user2.side });
+
+  if (rooms.filter((item) => item.room === user1.room)) {
+    const sideX = user1.side === "X" ? user1.id : user2.id;
+    const sideO = user1.side === "O" ? user1.id : user2.id;
+
+    rooms.push({
+      room: user1.room,
+      sideX,
+      sideO,
+      currTurn: sideX,
+      field: {
+        "cell-00": null,
+        "cell-01": null,
+        "cell-02": null,
+        "cell-10": null,
+        "cell-11": null,
+        "cell-12": null,
+        "cell-20": null,
+        "cell-21": null,
+        "cell-22": null,
+      },
+    });
+  }
+
+  const turnPass = rooms.find((room) => room.room === user1.room).currTurn;
+  io.to(turnPass).emit("yourTurn");
+}
+
+function Turn(socket, data) {
+  const { row, cell } = data;
+  //находим ид нажатой клетки
+  const clickedCellId = "cell-" + row + cell;
+  //находим комнату
+  const room = rooms.find((room) => room.currTurn === socket.id);
+  console.log(rooms.currTurn);
+  console.log(socket.id);
+  console.log(room);
+
+  if (room) {
+    //определяем текущего ходящего
+    const currTurn = room.currTurn === room.sideX ? room.sideX : room.sideO;
+    //определяем следующего ходящего
+    const nextTurn = room.currTurn === room.sideX ? room.sideO : room.sideX;
+    //определяем сторону ходящего
+    const side = room.sideX === socket.id ? "X" : "O";
+    //записываем ход на сервере
+    if (room.field[clickedCellId] === null) {
+      room.field[clickedCellId] = side;
+      room.currTurn = nextTurn;
+
+      io.to(currTurn).emit("yourTurn", { turn: "Opponent turn" });
+      io.to(nextTurn).emit("yourTurn", { turn: "Your turn" });
+
+      io.to([room.sideX, room.sideO]).emit("fieldUpdate", room.field);
+    }
+  } else {
+    console.log("opponent turn");
+  }
+}
+
+function SendMessage(socket, data) {
+  const { username, room, message, timestamp } = data;
+
+  io.in(room).emit("messageReceive", {
+    message,
+    username,
+    timestamp,
+  });
+}
+
+function socketDisconnect(socket) {
+  console.log("user:", socket.id, "disconnected");
+  allUsers = allUsers.filter((user) => socket.id !== user.id);
+  rooms = rooms.filter(
+    (item) => item.sideX !== socket.id && item.sideO !== socket.id
+  );
+  console.log(rooms);
+}
+
 /**
  * @desc массив, который хранит всех пользователей.
  * Внутри находятся объекты типа {id: socket.id, username, room, side: "X" || "O"}
@@ -40,173 +207,12 @@ let allUsers = [];
  * 'cell-10':null, 'cell-11':null, 'cell-12':null,
  * 'cell-20':null, 'cell-21':null, 'cell-22':null}}
  */
-let rooms = [
-  {
-    room: "sss",
-    sideX: "dsa",
-    sideO: "sad",
-    currTurn: "dsa",
-    field: {
-      "cell-00": null,
-      "cell-01": null,
-      "cell-02": null,
-      "cell-10": null,
-      "cell-11": null,
-      "cell-12": null,
-      "cell-20": null,
-      "cell-21": null,
-      "cell-22": null,
-    },
-  },
-];
+let rooms = [];
 
 const start = () => {
   try {
     io.on("connection", (socket) => {
-      console.log("user:", socket.id, "connected");
-
-      socket.on("joinRoom", (data) => {
-        const { username, room } = data;
-
-        const usersInRoom = allUsers.filter((user) => user.room === room);
-
-        if (usersInRoom.length < 2) {
-          //add a button to pick x or o
-
-          socket.join(room);
-          allUsers.push({ id: socket.id, username, room });
-
-          const timestamp = Date.now();
-
-          //сообщение всем участникам комнаты, что юзер присоединился
-          socket.to(room).emit("messageReceive", {
-            message: `${username} has joined`,
-            username: "server",
-            timestamp,
-          });
-
-          //приветствие
-          socket.emit("messageReceive", {
-            message: `Welcome ${username}`,
-            username: "server",
-            timestamp,
-          });
-        } else {
-          socket.emit("Error", { message: "can't join the room" });
-        }
-
-        if (allUsers.filter((user) => user.room === room).length === 2) {
-          const usersInRoom = allUsers.filter((user) => user.room === room);
-          console.log(usersInRoom);
-          const user1 = usersInRoom[0];
-          const user2 = usersInRoom[1];
-          io.to([user1.id, user2.id]).emit("showPickASideComponent");
-        }
-
-        socket.on("pickASide", (data) => {
-          const user1 = allUsers.filter(
-            (user) => user.room === room && user.id === socket.id
-          )[0];
-          const user2 = allUsers.filter(
-            (user) => user.room === room && user.id !== socket.id
-          )[0];
-          user1.side = data.side;
-
-          switch (data.side) {
-            case "X":
-              user2.side = "O";
-              break;
-            case "O":
-              user2.side = "X";
-              break;
-          }
-
-          io.to([user1.room, user2.room]).emit("showPickASideComponent");
-          io.to(user1.id).emit("sidePick", { side: user1.side });
-          io.to(user2.id).emit("sidePick", { side: user2.side });
-
-          if (rooms.filter((item) => item.room === user1.room)) {
-            const sideX = user1.side === "X" ? user1.id : user2.id;
-            const sideO = user1.side === "O" ? user1.id : user2.id;
-
-            rooms.push({
-              room: user1.room,
-              sideX,
-              sideO,
-              currTurn: sideX,
-              field: {
-                "cell-00": null,
-                "cell-01": null,
-                "cell-02": null,
-                "cell-10": null,
-                "cell-11": null,
-                "cell-12": null,
-                "cell-20": null,
-                "cell-21": null,
-                "cell-22": null,
-              },
-            });
-          }
-
-          const turnPass = rooms.find(
-            (room) => room.room === user1.room
-          ).currTurn;
-          io.to(turnPass).emit("yourTurn");
-
-          socket.emit("devInfo", allUsers);
-        });
-
-        socket.on("turn", (data) => {
-          const { row, cell } = data;
-          const clickedCellId = "cell-" + row + cell;
-          const room = rooms.find((room) => room.currTurn === socket.id);
-          console.log(room);
-          const side = room.sideX === socket.id ? "X" : "O";
-
-          const nextTurn =
-            room.currTurn === room.sideX ? room.sideO : room.sideX;
-
-          if (room) {
-            room.field[clickedCellId] = side;
-            room.currTurn = nextTurn;
-
-            const turnPass = rooms.find(
-              (room) => room.sideX === socket.id || room.sideO === socket.id
-            ).currTurn;
-            io.to(turnPass).emit("yourTurn");
-
-            io.to([room.sideX, room.sideO]).emit("fieldUpdate", room.field);
-          } else {
-            console.log("opponent turn");
-          }
-        });
-
-        socket.on("sendMessage", (data) => {
-          const { username, room, message, timestamp } = data;
-
-          io.in(room).emit("messageReceive", {
-            message,
-            username,
-            timestamp,
-          });
-          socket.emit("devInfo", allUsers);
-        });
-
-        //devInfo
-        socket.on("devInfo", () => {
-          socket.emit("devInfo", allUsers);
-          console.log(allUsers);
-        });
-      });
-
-      socket.on("disconnect", () => {
-        console.log("user:", socket.id, "disconnected");
-        allUsers = allUsers.filter((user) => socket.id !== user.id);
-        rooms = rooms.filter(
-          (item) => item.sideX !== socket.id && item.sideO !== socket.id
-        );
-        console.log(rooms);
-      });
+      socketConnect(socket);
     });
 
     app.get("*", (req, res) => {
--- src/components/SquareField/index.tsx
+++ src/components/SquareField/index.tsx
@@ -2,18 +2,51 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import { socket } from "../../socket";
 
+type Cell =
+  | "cell-00"
+  | "cell-01"
+  | "cell-02"
+  | "cell-10"
+  | "cell-11"
+  | "cell-12"
+  | "cell-20"
+  | "cell-21"
+  | "cell-22";
+
 export default function SquareField(): JSX.Element {
-  const [turn, setTurn] = useState<string>();
+  const [turn, setTurn] = useState<string>("");
+  const [field, setField] = useState<Record<Cell, null | "X" | "O">>({
+    "cell-00": null,
+    "cell-01": null,
+    "cell-02": null,
+    "cell-10": null,
+    "cell-11": null,
+    "cell-12": null,
+    "cell-20": null,
+    "cell-21": null,
+    "cell-22": null,
+  });
 
   const size: number[] = [0, 1, 2];
 
-  useEffect(() => {
-    socket.on("yourTurn", () => {
+  function handleTurn() {
+    if (turn === "Your turn") {
+      setTurn("Opponent turn");
+    } else {
       setTurn("Your turn");
+    }
+  }
+
+  useEffect(() => {
+    socket.on("yourTurn", (data) => {
+      console.log(data);
+
+      setTurn(data.turn);
     });
 
     socket.on("fieldUpdate", (data) => {
       console.log(data);
+      setField(data);
     });
   }, [socket]);
 
@@ -25,18 +58,27 @@ export default function SquareField(): JSX.Element {
 
   return (
     <div className="field">
-      {turn}
+      <h2 onClick={() => handleTurn()}>{turn}</h2>
       <div className="mainSquare">
         {size.map((row) => (
           <div className="cell-row">
-            {size.map((cell) => (
-              <div
-                data-row={row}
-                data-cell={cell}
-                className={`cell cell-${row}-${cell}`}
-                onClick={(e) => makeTurn(e)}
-              ></div>
-            ))}
+            {size.map((cell) => {
+              const cellId: Cell = `cell-${row}${cell}` as Cell;
+
+              return (
+                <div
+                  data-row={row}
+                  data-cell={cell}
+                  className={`cell cell-${row}-${cell}`}
+                  onClick={(e) => makeTurn(e)}
+                >
+                  {field[cellId] === "X" ? <div className="cross"></div> : null}
+                  {field[cellId] === "O" ? (
+                    <div className="circle"></div>
+                  ) : null}
+                </div>
+              );
+            })}
           </div>
         ))}
       </div>
