d2e0c435470c506835dc6087e9f93aef6d667eea
CyberCaban <andrewgamer63@gmail.com>
2023-11-12 20:33:57 +0300

    port 4

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const SERVER_URL = "http://localhost:4000";
+const SERVER_URL = window.location.href + ":4000";
 export const socket = io(SERVER_URL, {
   autoConnect: false,
 });
