77b81cbebb5ba779cc5bdfd529b1b3b571f716fd
Dmitry <andrewgamer63@gmail.com>
2023-11-18 22:58:04 +0300

    port 13

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const SERVER_URL = "https://tick-tack-toe-lyart.vercel.app/";
+const SERVER_URL = "https://tick-tack-toe-lyart.vercel.app:3000";
 export const socket = io(SERVER_URL, {
   autoConnect: false,
 });
