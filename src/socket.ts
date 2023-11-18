35b756e13e6a911124fabcb5a91700f69bc3aaff
Dmitry <andrewgamer63@gmail.com>
2023-11-18 21:41:01 +0300

    port 9

--- src/socket.ts
+++ src/socket.ts
@@ -2,5 +2,5 @@ import io from "socket.io-client";
 
 const SERVER_URL = "https://tick-tack-toe-cybercaban.vercel.app";
 export const socket = io(SERVER_URL, {
-  autoConnect: true,
+  autoConnect: false,
 });
