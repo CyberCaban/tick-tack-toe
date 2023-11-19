8dfc2e20fe05cdae54d8dd57c2e611bb7ddd81c0
CyberCaban <andrewgamer63@gmail.com>
2023-11-19 19:55:47 +0300

    server url

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
 //в проде заменить на https://tick-tack-toe.onrender.com/
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "https://tick-tack-toe.onrender.com/";
 export const socket = io(SERVER_URL, { autoConnect: false });
